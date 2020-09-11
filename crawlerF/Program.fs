0f8ed4d5d1875fc4f6f381da3ecd200b26390e28
Bot Maker <chooseusername9@protonmail.com>
2020-09-11 18:22:17 -0500

    - Got multicore support implemented and reading in commandline params.

--- crawlerF/Program.fs
+++ crawlerF/Program.fs
@@ -11,22 +11,12 @@ open FSharp.Data
 let main (args: string[]) =
     printfn "Arguments passes in: %A" args
     // F# searches the following directory at runtime : C:\Users\userName\source\repos\crawlerF\crawlerF\bin\Debug\netcoreapp2.1\test.csv'.
-    // It is therefore necessary to provide the top level of the repo for the file read by prepending ../../../../ to the file name    
-    let topDir = "../../../../"
-    let nestingLevel = 2
-    let filePath = "../../../../" + args.[1]    
-    let baseUrl1 = "https://scrapethissite.com"
-    let baseUrl2 = "https://cdc.gov"
-    let https = "https://"
+    // It is therefore necessary to provide the top level of the repo for the file read by prepending ../../../../ to the file name 
+    let filePath = "../../../../" + args.[1]
     let illegalChars = "<>:\"/\\|?*=."
 
-    let strPrint (str : string) : string =
-        printfn "After Distinct: %s" str
-        str
-
     let stripChars = 
-        String.map (fun s -> if Seq.exists((=)s) illegalChars then '_' else s) 
-    
+        String.map (fun s -> if Seq.exists((=)s) illegalChars then '_' else s)     
     
     let savePage (url : string, page : HtmlDocument) =
         let fileName = stripChars url
@@ -46,7 +36,7 @@ let main (args: string[]) =
             |> List.map(fun a -> a.AttributeValue("href"))
             |> List.distinctBy id
             |> List.filter (fun x -> not(x.Contains "http"))
-            |> List.map (fun x -> baseUrl1 + x) 
+            |> List.map (fun x -> page + x) 
             |> List.map (fun x -> 
                 match nestingLevel with
                 | _ when (nestingLevel > 0) -> crawlPage(x, (nestingLevel - 1)) 
@@ -54,25 +44,29 @@ let main (args: string[]) =
             |> List.concat
             |> List.distinctBy id
         with
-        | :? System.Net.WebException -> printfn "Page: %s caused exception" page; List.empty
         | :? System.Exception -> printfn "Page: %s caused exception" page; List.empty
-
-    let saw = crawlPage(baseUrl1, nestingLevel)
-    saw |> List.iter (fun x -> printfn "Look Here -> %s" x)
-
-    // Create navigable links out of the retrieved attribute strings
    
-    let urlList (filePath:string) = seq {
+    let urlList (filePath:string) = 
         use sr = new StreamReader (filePath)
-        while not sr.EndOfStream do
-            yield sr.ReadLine
-    }
-    
+        let arry = 
+            sr.ReadToEnd()
+            |> fun x -> x.Split([|','|])
+        Seq.ofArray(arry)    
 
-    //urlList(filePath)
-    //|> printfn "Here %A"
+    let asyncCrawl (url : string) = 
+        async {
+           crawlPage(url, args.[0] |> int) |> ignore
+        }
+        
+    let targetUrls = urlList(filePath)
+    targetUrls |> Seq.map (fun x -> printfn "target url: %s" x) |> ignore
+
+    targetUrls 
+    |> Seq.map (fun x -> asyncCrawl x) 
+    |> Seq.map (fun x -> Async.StartAsTask x) 
+    |> System.Threading.Tasks.Task.WhenAll
+    |> ignore
     
-    
-    printfn "END"
+    printfn "Press any key to exit..."
     Console.ReadKey() |> ignore
     0 // return an integer exit code
