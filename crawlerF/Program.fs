73407700e559c0619d9314f71abb85c47eb3fb4f
Bot Maker <chooseusername9@protonmail.com>
2020-09-11 20:16:34 -0500

    - Stripping extra / from page url before concatenating next level url.

--- crawlerF/Program.fs
+++ crawlerF/Program.fs
@@ -15,16 +15,21 @@ let main (args: string[]) =
     let filePath = "../../../../" + args.[1]
     let illegalChars = "<>:\"/\\|?*=."
 
+    // Strip illegal file name characters
     let stripChars = 
         String.map (fun s -> if Seq.exists((=)s) illegalChars then '_' else s)     
+
     
+    
+    // Create a file and save the page's html
     let savePage (url : string, page : HtmlDocument) =
         let fileName = stripChars url
-        printfn "File Created: %s" fileName
+        //printfn "File Created: %s" fileName
         use streamWriter = new StreamWriter("../../../SavedPages/" + fileName + ".txt")
         streamWriter.WriteLine(page.ToString())
         page   
 
+    // Crawl the provided page
     let rec crawlPage (page : String, nestingLevel : int) : list<string> =    
         System.Threading.Thread.Sleep 1200
         printfn "Crawling URL: %s" page
@@ -36,7 +41,10 @@ let main (args: string[]) =
             |> List.map(fun a -> a.AttributeValue("href"))
             |> List.distinctBy id
             |> List.filter (fun x -> not(x.Contains "http"))
-            |> List.map (fun x -> page + x) 
+            |> List.filter (fun x -> not(x.StartsWith('#'))) // The CDC webstie has a lot of these
+            |> List.filter (fun x -> not(String.length(x) = 1))
+            //|> List.map (fun x -> page + x) 
+            |> List.map (fun x -> page.TrimEnd('/') + x) 
             |> List.map (fun x -> 
                 match nestingLevel with
                 | _ when (nestingLevel > 0) -> crawlPage(x, (nestingLevel - 1)) 
@@ -46,20 +54,25 @@ let main (args: string[]) =
         with
         | :? System.Exception -> printfn "Page: %s caused exception" page; List.empty
    
-    let urlList (filePath:string) = 
+    // Url list read from provided csv
+    let urlList (filePath : string) = 
         use sr = new StreamReader (filePath)
         let arry = 
             sr.ReadToEnd()
             |> fun x -> x.Split([|','|])
         Seq.ofArray(arry)    
 
+    // Async wrapper function to run the crawlPage asynchronously 
     let asyncCrawl (url : string) = 
         async {
            crawlPage(url, args.[0] |> int) |> ignore
         }
         
     let targetUrls = urlList(filePath)
-    targetUrls |> Seq.map (fun x -> printfn "target url: %s" x) |> ignore
+    //targetUrls |> Seq.map (fun x -> printfn "target url: %s" x) |> ignore
+    let str1 = "test/"
+    let str2 = str1.TrimEnd('/')
+    printfn "HEY: %s" str2
 
     targetUrls 
     |> Seq.map (fun x -> asyncCrawl x) 
